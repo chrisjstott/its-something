0d70a188795d4ee9182d2e8206c47b4b05c1ad0a
Chris Stott <cstott@johnmcneilstudio.com>
2017-02-26 00:04:59 -0800

    tweaks

--- src/app.js
+++ src/app.js
@@ -20,16 +20,14 @@ window.addEventListener('scroll', tilt);
 let randomDegs = [];
 let randomYs = [];
 for (let i = 0; i < fallingLines.length; i++) {
-  randomDegs[i] = (Math.random() * 36) - 18;
+  randomDegs[i] = (Math.random() * 20) - 10;
   randomYs[i] = i < fallingLines.length - 1 ? Math.random() * -30 : -30;
 }
 
 function tilt() {
   let w = (falling.getBoundingClientRect().top - (window.innerHeight - falling.getBoundingClientRect().height)/2) / window.innerHeight;
   
-  if (w < 0) {
-    for (let i = 0; i < fallingLines.length; i++) {
-      fallingLines[i].style.transform = 'rotateZ(' + (w * randomDegs[i]) + 'deg) translateY(' + (w * randomYs[i]) + 'vh)';
-    }
+  for (let i = 0; i < fallingLines.length; i++) {
+    fallingLines[i].style.transform = 'rotateZ(' + (Math.min(w,0) * randomDegs[i]) + 'deg) translateY(' + (Math.min(w,0) * randomYs[i]) + 'vh)';
   }
 }
