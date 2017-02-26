b2d5240cfb39779473b39417643a71c69a65c340
Chris Stott <cstott@johnmcneilstudio.com>
2017-02-25 23:55:46 -0800

    poem

--- src/app.js
+++ src/app.js
@@ -12,3 +12,24 @@ let titles = document.querySelectorAll('.title-line');
 for (let title of titles) {
   letterWrap(title);
 }
+
+let falling = document.querySelector('.falling');
+let fallingLines = falling.querySelectorAll('.falling-line');
+window.addEventListener('scroll', tilt);
+
+let randomDegs = [];
+let randomYs = [];
+for (let i = 0; i < fallingLines.length; i++) {
+  randomDegs[i] = (Math.random() * 36) - 18;
+  randomYs[i] = i < fallingLines.length - 1 ? Math.random() * -30 : -30;
+}
+
+function tilt() {
+  let w = (falling.getBoundingClientRect().top - (window.innerHeight - falling.getBoundingClientRect().height)/2) / window.innerHeight;
+  
+  if (w < 0) {
+    for (let i = 0; i < fallingLines.length; i++) {
+      fallingLines[i].style.transform = 'rotateZ(' + (w * randomDegs[i]) + 'deg) translateY(' + (w * randomYs[i]) + 'vh)';
+    }
+  }
+}
